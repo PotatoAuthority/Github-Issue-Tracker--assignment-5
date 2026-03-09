0778b1cfbdb59bc663357249eb38ace249257c33
PotatoAuthority <ahsanmanjurul27@gmail.com>
2026-03-09 20:45:44 +0600

    card created

--- all_issues.js
+++ all_issues.js
@@ -18,16 +18,29 @@ const generateCard = (issue_in) =>{
 
     issue_in.data.forEach(elem =>{
         const issue_card = document.createElement('div');
+
+        const createdDate = elem.createdAt;
+        const date = new Date(createdDate);
+        const formattedDate = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+
+        console.log(formattedDate); // Output: "1/15/2024"
         issue_card.innerHTML = `
-        <div class="top flex justify-between items-center">
+        
+                    <div class="top flex justify-between items-center mb-3">
                         <img src="${elem.status==='open'? './asset/Open-Status.png' :'./asset/Closed- Status .png'}" alt="">
                         ${elem.priority === 'high' ? `<div class="badge badge-soft badge-error">HIGH</div>` : elem.priority === 'medium' ? `<div class="badge badge-soft badge-warning">MEDIUM</div>` : `<div class="badge badge-soft badge-success">LOW</div>`}
                     </div>
-                    <p>${elem.id}</p>
-                    <h2 class="font-semibold text-[14px] line-clamp-2 h-10">${elem.title}</h2>
-                    <p class="text-[12px] text-[#64748B] line-clamp-2 ">${elem.description}</p>
-                    <hr>
-                    <div class="flex flex-wrap gap-1">${elem.labels.map(label => `<div class="badge badge-outline badge-error text-[12px] font-medium">${label}</div>`).join("")}</div>
+                    
+                    <div>
+                        <h2 class="font-semibold text-[14px] line-clamp-2 h-10 mb-2">${elem.title}</h2>
+                        <p class="text-[12px] text-[#64748B] line-clamp-2 mb-3">${elem.description}</p>
+                    </div>
+                    
+                    <div class="flex flex-wrap gap-1">${elem.labels.map(label => `<div class="badge badge-outline badge-error text-[12px] font-medium p-1">${label}</div>`).join("")}</div>
+                    <hr class="mt-4 mb-4">
+                    <p class="text-[12px] text-[#64748B] line-clamp-2 mb-3">#1 by ${elem.author}</p>
+                    <p class="text-[12px] text-[#64748B] line-clamp-2 ">${formattedDate}</p>
+                    
                    
         `;
         // issue_card.classList.add('border-2');
